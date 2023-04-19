afd8a977a3c6be01192e7b3fe2e8a08aaa68b141
Vipul Rai <vipulrai8891@gmail.com>
2023-04-20 01:15:32 +0200

    undirected traversal avoid cyclic loop

--- graph-algos/4_undirected_traversal.py
+++ graph-algos/4_undirected_traversal.py
@@ -1,10 +1,16 @@
+# This is multiple problem program
+# 1. Convert Edges to Graph
+# 2. Traverse to find the edges
+# 3. Make sure to add visited flag to avoid cyclic graph and infinte loop
+
+
 from collections import defaultdict
 
 undirected_edges = [["i", "j"], ["k", "i"], ["m", "k"], ["k", "l"], ["o", "n"]]
 
 
-def helper_buildGraph(edges: list[list]) -> dict:
-    """Helper function to return egdes as graph
+def helper_build_Graph(edges: list[list]) -> dict:
+    """Helper function to return egdes as graph , creates adjacency list(dict in this case)
 
     Args:
         edges (list[list]): undirected edges 
@@ -20,5 +26,27 @@ def helper_buildGraph(edges: list[list]) -> dict:
     return dict(graph)
 
 
-graph = helper_buildGraph(undirected_edges)
-print(graph)
+def hasPath(graph, src, dest, visited):
+    # base case
+    if src == dest:
+        return True
+    # check visited to avoid acyclic graph
+    if src in visited:
+        return False
+    visited.add(src)
+
+    for neighbors in graph[src]:
+        if hasPath(graph, neighbors, dest, visited) == True:
+            return True
+
+    return False
+
+
+def undirectedPath(edges, nodeA, nodeB):
+    graph = helper_build_Graph(edges)
+    return hasPath(graph, nodeA, nodeB, set())
+
+
+nodeA = "j"
+nodeB = "m"
+print(undirectedPath(undirected_edges, nodeA, nodeB))
