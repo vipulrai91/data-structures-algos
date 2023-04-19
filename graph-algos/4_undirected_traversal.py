61c0733d2d3ff7382c12357a37a819060941f904
Vipul Rai <vipulrai8891@gmail.com>
2023-04-20 00:52:34 +0200

    edge to graph helper function

--- graph-algos/4_undirected_traversal.py
+++ graph-algos/4_undirected_traversal.py
@@ -1,6 +1,24 @@
-edges = {["i", "j"], ["k", "i"], ["m", "k"], ["k", "l"], ["o", "n"]}
+from collections import defaultdict
 
-graph = {
-"i" : "j",
+undirected_edges = [["i", "j"], ["k", "i"], ["m", "k"], ["k", "l"], ["o", "n"]]
 
-}
+
+def helper_buildGraph(edges: list[list]) -> dict:
+    """Helper function to return egdes as graph
+
+    Args:
+        edges (list[list]): undirected edges 
+
+    Returns:
+        dict: graph
+    """
+    graph = defaultdict(list)
+    for edge in edges:
+        a, b = edge
+        graph[a].append(b)
+        graph[b].append(a)
+    return dict(graph)
+
+
+graph = helper_buildGraph(undirected_edges)
+print(graph)
